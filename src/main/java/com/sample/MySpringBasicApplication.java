54d1dd3c864d921dd36118ff85dc161f002b8159
takumin4811 <t-nishimura@nri.co.jp>
2017-02-20 22:59:52 +0900

    a little little modify

--- src/main/java/com/sample/MySpringBasicApplication.java
+++ src/main/java/com/sample/MySpringBasicApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MySpringBasicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MySpringBasicApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MySpringBasicApplication.class, args);
+  }
 }
